b6c3ff2c48d1c282019dc46a8807450e47cd4788
ecoologic <erikecoologic@gmail.com>
2013-08-10 09:28:15 +1000

    ng-repeat

--- js/app.js
+++ js/app.js
@@ -2,5 +2,8 @@
 
 function TodoCtrl($scope) {
   $scope.totalTodos = 4;
-
+  $scope.todos = [
+    {text: "watch first angular video", done: false},
+    {text: "reproduce the code", done:false}
+  ];
 };
