510d39c28aaee268c6c943e77014f6e10b13f7e0
ecoologic <erikecoologic@gmail.com>
2013-08-10 10:57:20 +1000

    ANG: bind a calc value

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 // app
 
 function TodoCtrl($scope) {
-  $scope.totalTodos = 4;
   $scope.todos = [
     {text: "watch first angular video", done: false},
     {text: "reproduce the code", done:false}
   ];
+
+  $scope.getTotalTodos = function() {
+    return $scope.todos.length;
+  };
+
   $scope.addTodo = function() {
     $scope.todos.push({text: $scope.formTodoText, done: false});
     $scope.formTodoText = '';
